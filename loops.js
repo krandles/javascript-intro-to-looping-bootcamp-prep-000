c686a1e9d2746889a2e31c6ed7c564552a56b81b
Kevin Randles <krandles@gmail.com>
2017-10-31 10:52:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,3 +16,14 @@ function whileLoop(number) {
   }
   return 'done'
 }
+
+function doWhileLoop(array) {
+  do {
+    array.pop()
+  } while (array.length > 0 && maybeTrue());
+  return array
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
